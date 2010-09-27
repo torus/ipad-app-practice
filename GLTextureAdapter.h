321ac02b65323a835653df9ee3a50a7ae2109cd3
Toru Hisai <toru-github@torus.jp>
2010-09-26 16:16:02 -1000

    added C++ wrapper class for GLTexture

---
+++ GLTextureAdapter.h
@@ -0,0 +1,18 @@
+/*
+ *  GLTexureAdapter.h
+ *  Box2DPractice
+ *
+ *  Created by Toru Hisai on 10/09/26.
+ *  Copyright 2010 Kronecker's Delta Studio. All rights reserved.
+ *
+ */
+
+    // This header file must be pure C++ for SWIG
+
+class GLTextureAdapter {
+    void *entity;
+public:
+    GLTextureAdapter(const char* filename);
+    ~GLTextureAdapter();
+    void draw(double x, double y, double rot, double scale);
+};
---
+++ GLTextureAdapter.mm
@@ -0,0 +1,37 @@
+/*
+ *  GLTextureAdapter.mm
+ *  Box2DPractice
+ *
+ *  Created by Toru Hisai on 10/09/26.
+ *  Copyright 2010 Kronecker's Delta Studio. All rights reserved.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+#import "GLTexture.h"
+#import "GLTextureAdapter.h"
+
+GLTextureAdapter::GLTextureAdapter(const char* filename)
+{
+    NSString *fullpath = [[[NSBundle mainBundle] bundlePath]
+                          stringByAppendingPathComponent:[NSString stringWithCString:filename encoding:NSUTF8StringEncoding]];
+    NSLog(@"Texture path: %@", fullpath);
+    
+    UIImage *loadImage = [UIImage imageWithContentsOfFile:fullpath];
+    NSLog(@"Image: %x", loadImage);
+    
+    entity = [[GLTexture alloc] initWithImage:loadImage];
+    
+    [loadImage dealloc];
+}
+
+GLTextureAdapter::~GLTextureAdapter()
+{
+    [(GLTexture*)entity dealloc];
+}
+
+void
+GLTextureAdapter::draw(double x, double y, double rot, double scale)
+{
+    [(GLTexture*)entity drawAtPoint:CGPointMake(x, y) withRotation:rot withScale:scale];
+}
