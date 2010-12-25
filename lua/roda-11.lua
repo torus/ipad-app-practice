c3a38d70e59132bcee03e606d67b88f39fb4cc39
Toru Hisai <toru-github@torus.jp>
2010-12-24 16:02:54 -1000

    refactoring

--- lua/roda-11.lua
+++ lua/roda-11.lua
@@ -14,7 +14,6 @@ setmetatable (x, m)
 print (tostring (glMatrixMode))
 print (tostring (GL_MODELVIEW))
 
-local tex1
 local world
 local edge_body
 local goya = {}
@@ -74,54 +73,57 @@ function C (...)
           end
 end
 
-local images
+function extract_page_data (images)
+   assert (type (images) == "table")
 
-local mat =
-   M ("images",
-      C (M ("#text"),
-         M ("image",
-            function (e)
-               local w = xmlGetProp (e, "width")
-               local h = xmlGetProp (e, "height")
+   local mat =
+      M ("images",
+         C (M ("#text"),
+            M ("image",
+               function (e)
+                  local w = xmlGetProp (e, "width")
+                  local h = xmlGetProp (e, "height")
 
-               local img = {name = e.name, width = w, height = h, tiles = {}}
-               print (img.name, img.width, img.height)
+                  local img = {name = e.name, width = w, height = h, tiles = {}}
+                  print (img.name, img.width, img.height)
 
-               local res = C (M ("#text"),
-                              M ("tile",
-                                 function (e)
-                                    local t = {
-                                       id = tonumber (xmlGetProp (e, "id")),
-                                       w = tonumber (xmlGetProp (e, "width")),
-                                       h = tonumber (xmlGetProp (e, "height")),
-                                       off_x = tonumber (xmlGetProp (e, "offset-x")) - 32,
-                                       off_y = tonumber (xmlGetProp (e, "offset-y")) - 32,
-                                    }
-                                    table.insert (img.tiles, t)
-                                    return true
-                                 end)) (e)
-               if res then
-                  table.insert (images, img)
-                  return true
-               else
-                  return false
-               end
-            end)))
+                  local res = C (M ("#text"),
+                                 M ("tile",
+                                    function (e)
+                                       local t = {
+                                          id = tonumber (xmlGetProp (e, "id")),
+                                          w = tonumber (xmlGetProp (e, "width")),
+                                          h = tonumber (xmlGetProp (e, "height")),
+                                          off_x = tonumber (xmlGetProp (e, "offset-x")) - 32,
+                                          off_y = tonumber (xmlGetProp (e, "offset-y")) - 32,
+                                       }
+                                       table.insert (img.tiles, t)
+                                       return true
+                                    end)) (e)
+                  if res then
+                     table.insert (images, img)
+                     return true
+                  else
+                     return false
+                  end
+               end)))
+   return mat
+end
 
 function proc ()
-   proc_init ("3-texture.png", getDir () .. "/3-compacted.xml")
+   local page = proc_init ("3-texture.png", getDir () .. "/3-compacted.xml")
    -- proc_init ("11-texture.png", getDir () .. "/11-compacted.xml")
 
    while true do
       if key_stat.swipe_right then
-         proc_init ("3-texture.png", getDir () .. "/3-compacted.xml")
+         page = proc_init ("3-texture.png", getDir () .. "/3-compacted.xml")
       elseif key_stat.swipe_left then
-         proc_init ("11-texture.png", getDir () .. "/11-compacted.xml")
+         page = proc_init ("11-texture.png", getDir () .. "/11-compacted.xml")
       end
 
       key_stat = {}
 
-      proc_draw ()
+      proc_draw (page)
       coroutine.yield ()
    end
 end
@@ -148,29 +150,33 @@ function proc_init (tex_path, data_path)
    glEnable(GL_BLEND)
    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-   tex1 = gltexture.GLTextureAdapter(tex_path)
+   local tex = gltexture.GLTextureAdapter(tex_path)
 
    print ("b2World: " .. tostring (b2World))
    print ("b2Vec2: " .. tostring (b2Vec2))
 
    world = b2World(b2Vec2(0, -10), false)
-   images = {}
+   local images = {}
    doc = xmlParseFile (data_path)
 
    local root = doc.children
    print ("root: " .. tostring (root))
 
-   local matresult = mat (root)
+   local matresult = extract_page_data (images) (root)
    print ("matresult: " .. tostring (matresult))
 
    print "proc_init() done"
+
+   return {tex = tex, images = images}
 end
 
 function draw ()
    coroutine.resume (proc_coroutine)
 end
 
-function proc_draw ()
+function proc_draw (page)
+   local tex, images = page.tex, page.images
+
    glClearColor(0.3, 0.3, 0.3, 1)
    glClear(GL_COLOR_BUFFER_BIT)
 
@@ -195,7 +201,7 @@ function proc_draw ()
          local id = tile.id
          local x = id % 32
          local y = (id - x) / 32
-         tex1:drawInRect(tile.off_x, 1024 - tile.off_y - 32, 0, x * 32, y * 32, 32, 32)
+         tex:drawInRect(tile.off_x, 1024 - tile.off_y - 32, 0, x * 32, y * 32, 32, 32)
       end
       glPopMatrix ()
    end
